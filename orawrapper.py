b8df8f18de1681fbe529dd178518fc8dde3b2571
Guior <guilhermeormond@hotmail.com>
2019-02-22 03:13:14 -0300

    Adicionado informações referentes ao funcionamento do projeto

--- orawrapper.py
+++ orawrapper.py
@@ -3,12 +3,18 @@
 # wrapper para python detectar o sistema operacional, e mandar os
 # dados para determinado programa em C por motivos de compatibilidade
 #
+# -> a bola possui trajetória fixa definida pelo arquivo trajetoria_1sem2019.txt
+# -> as coordenadas X e Y do robô serão definidas aleatoriamente durante a
+#    apresentação
+# -> o projeto todo utilizará como base apenas um robô para todo seu
+#    funcionamento: KidSize Humanoid ou Small Size
+# -> o objetivo do projeto resume-se em realizar o econtro do robô com a bola
+#    antes que ela saia do campo de futebol
 
 import sys # contém as funções necessárias para detectar o SO
 
-
 def main ():
-    sistema = sys.platform
+    sistema = sys.platform # checa o sistema operacional
     print("Rodando em %s" % sistema)
 
     if (sistema[:3] == "win"):
