96ec9a3902e8666946dd311eb3cbfcc403dfd75d
Guior <guilhermeormond@hotmail.com>
2019-03-25 00:43:14 -0300

    Adicionado cálculo de menor trajeto sem aceleração e plot em conjunto

--- linux/oralin.c
+++ linux/oralin.c
@@ -49,8 +49,8 @@ void definicaoPosRobo(elementos *r)
 
 }
 
-void definicaoDirRobo(int* mpx, int* mpy, 
-	              double robo_x0, double robo_y0, 
+void definicaoDirRobo(int* mpx, int* mpy,
+	              double robo_x0, double robo_y0,
 		      double bola_x0, double bola_y0)
 {
 	if (robo_x0 > bola_x0)
@@ -69,44 +69,45 @@ void definicaoDirRobo(int* mpx, int* mpy,
         		*mpy = 1;
 
 	}
-
-
 }
-double definicVelRobo(elementos *Robo, elementos *Bola, int iterador, float v_m)
+
+void definicTempoRobo(elementos *Robo, float *menorTempo,int iterador, float v_m)
 {
-	double velocidade, tempo_final;
-	tempo_final = Bola->tempo[iterador];
-	Robo->tempo[iterador] = tempo_final;
-	velocidade = Robo->distancia[iterador] / tempo_final;
-	if (velocidade < v_m)
-		return velocidade;
-	else
-		return 0;
-
+	double tempo_final;
+	tempo_final = Robo->distancia[iterador] / v_m;
+	if (tempo_final < *menorTempo)
+		*menorTempo = tempo_final;
 }
+
 void definicMovRobo(elementos *Bol, elementos *Rob, int *versor_x, int *versor_y, float raio_interc, float vel_max )
 {
 	// começamos por pitagoras
 	// distancia é a hipotenusa
 	double rob_x0 = Rob->x[LINHA_INIC];
 	double rob_y0 = Rob->y[LINHA_INIC];
-	double rob_xf, rob_yf, rob_vels[NUM_LINHAS];
+	double rob_xf, rob_yf, rob_tempos[NUM_LINHAS];
+	float menorTempo = 100;
 	int i;
 	// calcular todas as distancias possíveis
 	for(i = LINHA_INIC; i < NUM_LINHAS; i++)
 	{
 		rob_xf = Bol->x[i];
 		rob_yf = Bol->y[i];
-		Rob->distancia[i] = (sqrt(pow(( rob_xf - rob_x0 ), 2) + pow(( rob_yf - rob_y0), 2))) - raio_interc; 
-		rob_vels[i] = definicVelRobo(Rob, Bol,i, vel_max);
-		
+		Rob->distancia[i] = (sqrt(pow(( rob_xf - rob_x0)  , 2) + pow(( rob_yf - rob_y0), 2))) - raio_interc;
+		definicTempoRobo(Rob, &menorTempo, i, vel_max);
 	}
-	for(i = LINHA_INIC ; i < NUM_LINHAS ; i++)
-		if(Rob->tempo[i] != 0 && rob_vels[i] != 0) 
-			printf("Velocidades possíveis: %lf, distancia: %g, tempo: %g\n", rob_vels[i], Rob->distancia[i], Rob->tempo[i]);
 
-
-
+	for(i = LINHA_INIC ; i < NUM_LINHAS ; i++){
+		if ((menorTempo == Bol->tempo[i])) {
+			rob_xf = Bol->x[i];
+			rob_yf = Bol->y[i];
+		}
+		else if ((menorTempo > Bol->tempo[i]) && (menorTempo < Bol->tempo[i+1])) {
+			Rob->x[3] = Bol->x[i];
+			Rob->y[3] = Bol->y[i];
+		}
+	}
+	printf("O ponto de encontro da bola e o robô é X: %f, Y: %f\n", Rob->x[3], Rob->y[3]);
 }
 
 int main (void)
@@ -117,14 +118,14 @@ int main (void)
 	// para for loops
 	int i;
 	float raio_interc = raio_robo + raio_bola + dist_bounce; // bola sob o domínio do robo
-	
+
 	// zero os vetores da bola e robo
 	for(i = 0; i < 400; i++)
 		bola.tempo[i] = bola.x[i] = bola.y[i] = robo.x[i] = robo.y[i] = robo.tempo[i] = 0;
-		
+
 	// definiçao das posições iniciais do robo (vetor no indice 2)
 	definicaoPosRobo(&robo);
-	// define o versor para posição inicial, função será utilizada novamente 
+	// define o versor para posição inicial, função será utilizada novamente
 	definicaoDirRobo(&multipx, &multipy, robo.x[LINHA_INIC], robo.y[LINHA_INIC], bola.x[LINHA_INIC], bola.y[LINHA_INIC]);
 	int controle = leitura(bola.tempo, bola.x, bola.y); // funcao leitura le cada linha do arquivo, erros são guardados em controle
 	definicMovRobo(&bola, &robo, &multipy, &multipx, raio_interc, vel_robo);
@@ -133,16 +134,16 @@ int main (void)
 	// teste para ver dados da bola
 	//for(i = LINHA_INIC ; i < NUM_LINHAS ; i ++)
 	//	printf("tempo: %f x: %.8f y: %f\n ", bola.tempo[i], bola.x[i], bola.y[i]);
-	
+
 	// teste para ver posição inicial do robo
 	//	printf("robo: %f %f\n", robo.x[LINHA_INIC], robo.y[LINHA_INIC]);
-	
+
 	// teste para visualizar os versores
 	// 	printf("Versor em Y: %d Versor em X: %d\n", multipy, multipx);
 
 
 	// última função antes da finalização do programa
-	plotGraficos(bola.tempo, bola.x, bola.y, NUM_LINHAS);
+	plotGraficos(bola.tempo, bola.x, bola.y, robo.x, robo.y, NUM_LINHAS);
 
 	return 0;
 }
--- linux/plotar.h
+++ linux/plotar.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 
-void salvarCoordenadas(double bola_tempo[], double bola_x[], double bola_y[], int numlinhas){
+void salvarCoordenadas(double bola_tempo[], double bola_x[], double bola_y[],  double robo_x[], double robo_y[], int numlinhas){
   int i = 0;
   FILE * coordenadas; // ponteiro do tipo FILE
 
@@ -23,11 +23,16 @@ void salvarCoordenadas(double bola_tempo[], double bola_x[], double bola_y[], in
   for(i = 2; i < numlinhas; i++) // posição da bola Y em função do tempo
     fprintf(coordenadas, "%f %f\n", bola_tempo[i], bola_y[i]);
   fclose(coordenadas);
+
+  coordenadas = fopen("../recursos/coordenadas/trajetoriaRobo.txt", "w");
+  fprintf(coordenadas, "%f %f\n%f %f\n", robo_x[2], robo_y[2], robo_x[3], robo_y[3]);
+  fclose(coordenadas);
+
 }
 
-void plotGraficos(double bola_tempo[], double bola_x[], double bola_y[], int numlinhas)
+void plotGraficos(double bola_tempo[], double bola_x[], double bola_y[], double robo_x[], double robo_y[], int numlinhas)
 {
-	salvarCoordenadas(bola_tempo, bola_x, bola_y, numlinhas); // salva as coordenadas em um arquivo
+	salvarCoordenadas(bola_tempo, bola_x, bola_y, robo_x, robo_y, numlinhas); // salva as coordenadas em um arquivo
 
   system("gnuplot ../recursos/scripts/script1.gnu");
   system("gnuplot ../recursos/scripts/script2.gnu");
