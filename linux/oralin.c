5e2e34fbf601e2c30e092838e4055c5a7d5334dd
Guior <guilhermeormond@hotmail.com>
2019-04-26 03:16:06 -0300

    Limpeza no código, adição de safe input e corrigido erro de não-colisão

--- linux/oralin.c
+++ linux/oralin.c
@@ -18,7 +18,6 @@
  *
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -26,27 +25,40 @@
 #include "logar.h"
 #include "leitura.h"
 #include "plotar.h"
-// nosso header contendo a estrutura de medidas do robo e bola
 #include "elementos.h"
+
 #define NUM_LINHAS 367
 #define LINHA_INIC 2
 #define PI 3.141592653589793238462643383
-/*
- * Mais tardiamente, poderá ser adicionada a biblioteca SDL, a gameengine pedida
- */
+#define TEMPO_ITERACAO 0.02f
+#define NOT_FOUND 404
 
-// funcoes podem ser "jogadas" num header para mais limpeza
 void definicaoPosRobo(elementos *r, short int v)
 {
 	double x0, y0;
-	/*
-	 * Primeiro contato com usuário, serão pedidas as coordenadas x e y iniciais
-	 * Se possível, evitar utilizar scanf (buffer overflow), fgets() será implementado mais tarde
-	 */
-	// checar se usuário errou a entrada de dados
-	puts("Insira um valor para X e Y iniciais para o robô\n");
-	scanf("%lf", &x0);
-	scanf("%lf", &y0);
+	int xCopiados, yCopiados;
+	//Primeiro contato com usuário, serão pedidas as coordenadas x e y iniciais
+	do
+	{
+		puts("Insira um valor para X e Y iniciais para o robô\n");
+		char buffer[10];
+		char buffer2[10];
+
+		fgets(buffer, sizeof(buffer), stdin);
+		fgets(buffer2, sizeof(buffer2), stdin);
+
+		xCopiados = sscanf(buffer, "%lf", &x0);
+		yCopiados = sscanf(buffer2, "%lf", &y0);
+
+		if (xCopiados < 1 || yCopiados < 1) {
+			puts("Coordenadas inválidas, insira novamente");
+		}
+		else if (x0 < 0 || x0 > 9 || y0 < 0 || y0 > 6){
+			puts("Coordenadas inválidas, insira novamente");
+		}
+	}
+	while(xCopiados < 1 || yCopiados < 1 || x0 < 0 || x0 > 9 || y0 < 0 || y0 > 6);
+
 	r->x[LINHA_INIC] = x0;
 	r->y[LINHA_INIC] = y0;
 	if (v != 0)
@@ -62,11 +74,12 @@ double definicaoVelFinalRobo(double* deltaT, double* distIteracao, float porcent
 	return velFinal;
 }
 
-void definicaoDirRobo(int* mpx, int* mpy,
-	              double robo_x0, double robo_y0,
-		      double bola_x0, double bola_y0,
-		      short int v)
+void definicaoDirRobo(int* mpx, int* mpy, double robo_x0, double robo_y0, double bola_x0, double bola_y0, short int v)
 {
+	/*
+		Verifica a posição X e Y do robô em relação à bola em cada iteração
+		criando um versor +1 se estiver abaixo ou -1 se estiver em cima
+	*/
 	if (robo_x0 > bola_x0)
 	{
     		*mpx = -1;
@@ -100,28 +113,19 @@ void definicaoDirRobo(int* mpx, int* mpy,
 		logar("direcao", "Versor X", "Versor Y", *mpx, *mpy);
 }
 
-
 void definicAccelRobo(elementos *Robo, int i)
 {
-	Robo->acc.x[i] = (Robo->vel.x[i] - Robo->vel.x[i-1]) / 0.02;
-	Robo->acc.y[i] = (Robo->vel.y[i] - Robo->vel.y[i-1]) / 0.02;
-
-}
-
-void definicAccelBola(elementos *Bola, int i)
-{
-	Bola->acc.x[i] = 0.1092;
-	Bola->acc.y[i] = -0.04;
-
+	Robo->acc.x[i] = (Robo->vel.x[i] - Robo->vel.x[i-1]) / TEMPO_ITERACAO;
+	Robo->acc.y[i] = (Robo->vel.y[i] - Robo->vel.y[i-1]) / TEMPO_ITERACAO;
 }
 
 void definicVelRobo(elementos *Rob, elementos *Bol, int i, double comp_x, double comp_y, int accel, short int v  , double vel_f, double angulo)
 {
 
 	if(accel == 1){
-		Rob->vel.mod[i] = Rob->vel.mod[i-1] + (a_max * 0.02f);
+		Rob->vel.mod[i] = Rob->vel.mod[i-1] + (a_max * TEMPO_ITERACAO);
 	}else if(accel == -1){
-		Rob->vel.mod[i] = Rob->vel.mod[i-1] - (desa_max * 0.02f);
+		Rob->vel.mod[i] = Rob->vel.mod[i-1] - (desa_max * TEMPO_ITERACAO);
 	}else{
 		Rob->vel.mod[i] = vel_f;
 	}
@@ -135,16 +139,23 @@ void definicVelRobo(elementos *Rob, elementos *Bol, int i, double comp_x, double
 		Rob->vel.y[i] = Rob->vel.mod[i] * cos(angulo);
 	}
 
-
 	definicAccelRobo(Rob, i);
 	if ( v != 0){
 		logar("Velocidade", "Modulo velocidade", "", Rob->vel.mod[i], 0);
-
 		logar("Velocidade componentes", "VX", "VY", Rob->vel.x[i], Rob->vel.y[i]);
-	}}
+	}
+}
+
+void definicAccelBola(elementos *Bola, int i)
+{
+	//equações retiradas do Excel
+	Bola->acc.x[i] = 0.1092;
+	Bola->acc.y[i] = -0.04;
+}
 
 void definicVelBola(elementos *Bol, int i)
 {
+	//equações retiradas do Excel
 	Bol->vel.x[i] = (0.1092 * Bol->tempo[i]) + 0.3412;
 	Bol->vel.y[i] = (-0.04 * Bol->tempo[i]) + 0.9;
 
@@ -154,9 +165,7 @@ void definicVelBola(elementos *Bol, int i)
 float definicAngul(double rob_x0, double rob_xf, double rob_y0, double rob_yf, int* vers_x, int* vers_y, short int v)
 {
     float angulo    = 0.0f;
-    // x e y positivo, considerar a opção
-    // considerar y positivo
-
+		//definido atráves da tangente do delta x e y entre robô e bola
 		if(*vers_y < 0) //Robô acima da bola
 			angulo = atan2(fabs(rob_xf - rob_x0), fabs(rob_yf - rob_y0));
 		else if(*vers_y > 0) //Robô abaixo da bola
@@ -166,7 +175,7 @@ float definicAngul(double rob_x0, double rob_xf, double rob_y0, double rob_yf, i
 
   	if(v != 0)
   		logar("definicao angulo", "angulo", "", angulo * (180/PI), 0);
-      return angulo; //converte de radianos para graus
+      return angulo;
 }
 
 void defineComponentes(double* componenteX, double* componenteY, double modulo, float angulo, int* vers_x, int* vers_y, short int v) {
@@ -219,15 +228,14 @@ void defineComponentes(double* componenteX, double* componenteY, double modulo, 
 }
 
 void atualizaPosicao(elementos* Rob, double componenteX, double componenteY, int i){
-	//como dito, os componentes serão positivo ou negativo dependendo da direção, bastando somar
 	Rob->x[i] = Rob->x[i-1] + componenteX;
 	Rob->y[i] = Rob->y[i-1] + componenteY;
 }
 
 double distanciaRoboBola(double robX, double bolaX, double robY, double bolaY){
+	//pitágoras
 	return sqrt(pow((robX-bolaX), 2) + pow((robY-bolaY), 2));
 }
-
 
 int definicMovRobo(elementos *Bol, elementos *Rob, int* versor_x, int* versor_y, float raio_interc, float vel_max, double temp_desac, double distDesac, short int v, double vel_fin)
 {
@@ -247,7 +255,7 @@ int definicMovRobo(elementos *Bol, elementos *Rob, int* versor_x, int* versor_y,
 		Rob->tempo[i] = Bol->tempo[i];
 		soma_tempos = Bol->tempo[i];
 
-		accel 	= 1;
+		accel = 1;
 		definicaoDirRobo(versor_x, versor_y, Rob->x[i-1], Rob->y[i-1], Bol->x[i-1], Bol->y[i-1], v);
 
 		if(soma_tempos >= temp_accel && soma_tempos <= (temp_accel + temp_desac)){
@@ -258,9 +266,9 @@ int definicMovRobo(elementos *Bol, elementos *Rob, int* versor_x, int* versor_y,
 			distancia = distDesac;
 			accel = 0;
 		}
-    // definição do angulo
+
     angulo_temp = definicAngul(Rob->x[i-1], rob_xf, Rob->y[i-1], rob_yf, versor_x, versor_y, v );
-		//define componentes
+
 		defineComponentes(&componenteX, &componenteY, distancia, angulo_temp, versor_x, versor_y, v);
 		atualizaPosicao(Rob, componenteX, componenteY, i);
 
@@ -270,22 +278,24 @@ int definicMovRobo(elementos *Bol, elementos *Rob, int* versor_x, int* versor_y,
 		dist_relativa[i] = distanciaRoboBola(Rob->x[i], Bol->x[i], Rob->y[i], Bol->y[i]);
 
 		if( dist_relativa[i] <= raio_interc)
-			break;
+			return i;
 	}
-	return i;
+	return NOT_FOUND;
 }
 
 int main (int argc, char * argv[])
 {
 	elementos bola, robo;
-	robo.diam		 = 0.180f;
-	bola.diam	 	 = 0.046f;
-	robo.vel.x[2] 		 = 0.0;
-	robo.vel.y[2] 		 = 0.0;
-	short int verboso 	= 0;
+	short int verboso = 0;
 	double temp_desac;
 	double distIteracao;
 	float porcentagem = 0.05;
+	int i, pontoDeEncontro;
+	robo.diam = 0.180f;
+	bola.diam	= 0.046f;
+	robo.vel.x[2] = 0.0;
+	robo.vel.y[2] = 0.0;
+
 	if(argc == 2){
 		if(!(strcmp(argv[1], "-v")))
 				verboso = 1;
@@ -293,22 +303,23 @@ int main (int argc, char * argv[])
 	else
 		puts("Argumentos não reconhecidos, utilize \"-v\" para logs.");
 
-	int i, pontoDeEncontro;
-	float raio_interc = (robo.diam/2) + (bola.diam/2) + dist_bounce; // bola sob o domínio do robo
-
+	float raio_interc = (robo.diam/2) + (bola.diam/2) + dist_bounce;
 
 	if(verboso != 0)
 		fclose(fopen("../logs/log", "w"));
 	for(i = 0; i < 400; i++)
 		bola.tempo[i] = bola.x[i] = bola.y[i] = robo.x[i] = robo.y[i] = robo.tempo[i] = 0;
 
-	// definiçao das posições iniciais do robo (vetor no indice 2)
 	definicaoPosRobo(&robo, verboso);
 	int controle = leitura(bola.tempo, bola.x, bola.y); // funcao leitura le cada linha do arquivo, erros são guardados em controle
 	pontoDeEncontro = definicMovRobo(&bola, &robo, &multipy, &multipx, raio_interc, vel_robo, temp_desac, distIteracao, verboso, definicaoVelFinalRobo(&temp_desac, &distIteracao, porcentagem));
 
-	// última função antes da finalização do programa
-	plotGraficos(&bola, &robo, NUM_LINHAS, pontoDeEncontro, robo.diam, bola.diam);
+	if(pontoDeEncontro == NOT_FOUND) {
+		printf("O Robô não conseguiu colidir com a bola durante seu percurso\n");
+	}
+	else{
+		plotGraficos(&bola, &robo, NUM_LINHAS, pontoDeEncontro, robo.diam, bola.diam);
+	}
 
 	logar("Final programa", "erro?", "",controle , 0);
 	return controle;
